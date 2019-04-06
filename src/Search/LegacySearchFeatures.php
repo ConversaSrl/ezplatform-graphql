bdunogier/ezplatform-graphql-bundle

Handled different coverage of field criteria across search engines
The Legacy and Solr search engines don't support the same fields as field criteria.
This delegates checking for that to SearchFeature objects specialized depending on the engine type.

Search/LegacySearchFeatures.php
@@ -1,0 +1,23 @@
+<?php
+namespace BD\EzPlatformGraphQLBundle\Search;
+
+use eZ\Publish\API\Repository\Values\ContentType\FieldDefinition;
+use eZ\Publish\Core\Persistence\Legacy\Content\FieldValue\ConverterRegistry;
+
+class LegacySearchFeatures implements SearchFeatures
+{
+    /**
+     * @var \eZ\Publish\Core\Persistence\Legacy\Content\FieldValue\ConverterRegistry
+     */
+    private $converterRegistry;
+
+    public function __construct(ConverterRegistry $converterRegistry)
+    {
+        $this->converterRegistry = $converterRegistry;
+    }
+
+    public function supportsFieldCriterion(FieldDefinition $fieldDefinition)
+    {
+        return $this->converterRegistry->getConverter($fieldDefinition->fieldTypeIdentifier)->getIndexColumn() !== false;
+    }
+}

Search/SearchFeatures.php
@@ -1,0 +1,16 @@
+<?php
+namespace BD\EzPlatformGraphQLBundle\Search;
+
+use eZ\Publish\API\Repository\Values\ContentType\FieldDefinition;
+
+interface SearchFeatures
+{
+    /**
+     * Tests if search supports field filtering on $fieldDefinition.
+     *
+     * @param FieldDefinition $fieldDefinition
+     *
+     * @return bool
+     */
+    public function supportsFieldCriterion(FieldDefinition $fieldDefinition);
+}

Search/SolrSearchFeatures.php
@@ -1,0 +1,12 @@
+<?php
+namespace BD\EzPlatformGraphQLBundle\Search;
+
+use eZ\Publish\API\Repository\Values\ContentType\FieldDefinition;
+
+class SolrSearchFeatures implements SearchFeatures
+{
+    public function supportsFieldCriterion(FieldDefinition $fieldDefinition)
+    {
+        return true;
+    }
+}

ConversaSrl/ezplatform-graphql

src/Search/LegacySearchFeatures.php
@@ -1,0 +1,23 @@
+<?php
+namespace EzSystems\EzPlatformGraphQL\Search;
+
+use eZ\Publish\API\Repository\Values\ContentType\FieldDefinition;
+use eZ\Publish\Core\Persistence\Legacy\Content\FieldValue\ConverterRegistry;
+
+class LegacySearchFeatures implements SearchFeatures
+{
+    /**
+     * @var \eZ\Publish\Core\Persistence\Legacy\Content\FieldValue\ConverterRegistry
+     */
+    private $converterRegistry;
+
+    public function __construct(ConverterRegistry $converterRegistry)
+    {
+        $this->converterRegistry = $converterRegistry;
+    }
+
+    public function supportsFieldCriterion(FieldDefinition $fieldDefinition)
+    {
+        return $this->converterRegistry->getConverter($fieldDefinition->fieldTypeIdentifier)->getIndexColumn() !== false;
+    }
+}

src/Search/SearchFeatures.php
@@ -1,0 +1,16 @@
+<?php
+namespace EzSystems\EzPlatformGraphQL\Search;
+
+use eZ\Publish\API\Repository\Values\ContentType\FieldDefinition;
+
+interface SearchFeatures
+{
+    /**
+     * Tests if search supports field filtering on $fieldDefinition.
+     *
+     * @param FieldDefinition $fieldDefinition
+     *
+     * @return bool
+     */
+    public function supportsFieldCriterion(FieldDefinition $fieldDefinition);
+}

src/Search/SolrSearchFeatures.php
@@ -1,0 +1,12 @@
+<?php
+namespace EzSystems\EzPlatformGraphQL\Search;
+
+use eZ\Publish\API\Repository\Values\ContentType\FieldDefinition;
+
+class SolrSearchFeatures implements SearchFeatures
+{
+    public function supportsFieldCriterion(FieldDefinition $fieldDefinition)
+    {
+        return true;
+    }
+}